bfb51468409e58aa3e11135f11e1895218d32714
saja obeidat <161194413+Saja-Omar@users.noreply.github.com>
2024-12-18 07:38:14 +0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-let img=document.querySelector('.im');
-let container=document.querySelector('.container')
-function phones(phon){
-    img.src=phon;
-}
-function colors(color){
-    container.style.background=color;
-}
