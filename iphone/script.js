8f0b32260b92717b3281dbb0db4ca734799c691b
saja obeidat <161194413+Saja-Omar@users.noreply.github.com>
2024-12-18 07:39:56 +0300

    Add files via upload

---
+++ iphone/script.js
@@ -0,0 +1,8 @@
+let img=document.querySelector('.im');
+let container=document.querySelector('.container')
+function phones(phon){
+    img.src=phon;
+}
+function colors(color){
+    container.style.background=color;
+}
